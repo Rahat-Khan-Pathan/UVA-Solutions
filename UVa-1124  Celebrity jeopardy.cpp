a4037f516d7b761e118c33275be230ddf29157c6
Rahat-Khan-Pathan <85352119+Rahat-Khan-Pathan@users.noreply.github.com>
2021-06-04 19:34:00 +0600

    Add files via upload

---
+++ UVa-1124  Celebrity jeopardy.cpp
@@ -0,0 +1,33 @@
+/*
+||-----------------------------------||
+||        Rahat Khan Pathan          ||
+||      BGC Trust University         ||
+||       Department of CSE           ||
+||-----------------------------------||
+*/
+#include<bits/stdc++.h>
+#define ll long long int
+#define pub push_back
+#define pi 3.14159265359
+#define vll vector<ll>
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique(All(X)),(X).end()))
+#define fast ios_base::sync_with_stdio(0); cin.tie(0);
+#define mp make_pair
+#define vpair vector<pair<ll,ll>>
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define minus cout<<-1<<endl
+using namespace std;
+int main()
+{
+    fast;
+    ll a,b,c,d;
+    while(cin>>a>>b>>c>>d)
+    {
+        if(a==0 && b==0 && c==0 && d==0) break;
+        ll ans=360+360+((a-b+40)%40)*9+360+((c-b+40)%40)*9+((c-d+40)%40)*9;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
