c27fd10bf0247d4cf541198d512e2b9b7d18434d
Rahat-Khan-Pathan <85352119+Rahat-Khan-Pathan@users.noreply.github.com>
2021-06-06 15:23:50 +0600

    Add files via upload

---
+++ 10114 - Loansome Car Buyer.cpp
@@ -0,0 +1,57 @@
+/*
+||-----------------------------------||
+||        Rahat Khan Pathan          ||
+||      BGC Trust University         ||
+||       Department of CSE           ||
+||-----------------------------------||
+*/
+#include<bits/stdc++.h>
+#define ll long long int
+#define pub push_back
+#define pi 3.14159265359
+#define vll vector<ll>
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique(All(X)),(X).end()))
+#define fast ios_base::sync_with_stdio(0); cin.tie(0);
+#define mp make_pair
+#define vpair vector<pair<ll,ll>>
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define minus cout<<-1<<endl
+using namespace std;
+int main()
+{
+    fast;
+    ll month,n;
+    double downPayment,totalMoney;
+    while(cin>>month>>downPayment>>totalMoney>>n)
+    {
+        if(month<=0)
+            break;
+        ll x,i;
+        double percant;
+        double pay=totalMoney/(double)month;
+        double a[1005]={0};
+        for(i=0;i<n;i++)
+        {
+            cin>>x>>percant;
+            a[x]=percant;
+        }
+        double currentMoney=totalMoney;
+        totalMoney+=downPayment;
+        totalMoney-=(totalMoney*a[0]);
+        percant=a[0];
+        for(i=1;i<=month+1;i++)
+        {
+            if(totalMoney>currentMoney)
+            {
+                (i-1==1)? cout<<i-1<<" month"<<endl : cout<<i-1<<" months"<<endl;
+                break;
+            }
+            if(a[i]>0) percant=a[i];
+            currentMoney-=pay;
+            totalMoney-=(totalMoney*percant);
+        }
+    }
+    return 0;
+}
