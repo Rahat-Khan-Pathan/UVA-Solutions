af8856573809e3d53de033cb3becefe1defe81cb
Rahat-Khan-Pathan <85352119+Rahat-Khan-Pathan@users.noreply.github.com>
2021-06-06 16:56:04 +0600

    Add files via upload

--- 10300 - Ecological Premium.cpp
+++ 10300 - Ecological Premium.cpp
@@ -16,20 +16,29 @@
 #define fast ios_base::sync_with_stdio(0); cin.tie(0);
 #define mp make_pair
 #define vpair vector<pair<ll,ll>>
-#define yes cout<<"YES"<<endl
-#define no cout<<"NO"<<endl
+#define yes cout<<"yes"<<endl
+#define no cout<<"no"<<endl
 #define minus cout<<-1<<endl
 using namespace std;
 void solve()
 {
-    ll n,i,a,b,c,ans=0;
+    ll n,i,a,b;
     cin>>n;
-    for(i=0;i<n;i++)
+    cin>>a>>b;
+    ll df=abs(a-b);
+    bool ans=true;
+    for(i=1; i<n; i++)
     {
-        cin>>a>>b>>c;
-        ans+=(a*c);
+        cin>>a>>b;
+        if(abs(a-b)!=df)
+        {
+            ans=false;
+        }
     }
-    cout<<ans<<endl;
+    if(ans)
+        yes;
+    else
+        no;
 }
 int main()
 {
@@ -37,6 +46,10 @@ int main()
     ll t;
     cin>>t;
     while(t--)
+    {
         solve();
+        if(t)
+            cout<<endl;
+    }
     return 0;
 }
