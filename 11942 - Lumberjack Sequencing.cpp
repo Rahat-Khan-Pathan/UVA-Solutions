a479014be00b1145f73816cc791e75e47e09016f
Rahat-Khan-Pathan <85352119+Rahat-Khan-Pathan@users.noreply.github.com>
2021-06-07 14:52:27 +0600

    Add files via upload

---
+++ 11942 - Lumberjack Sequencing.cpp
@@ -0,0 +1,73 @@
+
+/*
+||-----------------------------------||
+||        Rahat Khan Pathan          ||
+||      BGC Trust University         ||
+||       Department of CSE           ||
+||-----------------------------------||
+*/
+#include<bits/stdc++.h>
+#define ll long long int
+#define pub push_back
+#define pi 3.14159265359
+#define vll vector<ll>
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique(All(X)),(X).end()))
+#define fast ios_base::sync_with_stdio(0); cin.tie(0);
+#define pi pair<ll,ll>
+#define vpair vector<pair<ll,ll> >
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define minus cout<<-1<<endl
+using namespace std;
+void solve()
+{
+    ll n=10,i;
+    ll a[n+1],b[n+1];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+        b[i]=a[i];
+    }
+    sort(a,a+n);
+    bool ans=true;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]!=b[i])
+        {
+            ans=false;
+            break;
+        }
+    }
+    if(ans)
+    {
+        cout<<"Ordered"<<endl;
+        return;
+    }
+    ans=true;
+    sort(a,a+n,greater<ll>());
+    for(i=0;i<n;i++)
+    {
+        if(a[i]!=b[i])
+        {
+            ans=false;
+            break;
+        }
+    }
+    if(ans)
+        cout<<"Ordered"<<endl;
+    else
+        cout<<"Unordered"<<endl;
+}
+int main()
+{
+    fast;
+    ll t;
+    cin>>t;
+    cout<<"Lumberjacks:"<<endl;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
