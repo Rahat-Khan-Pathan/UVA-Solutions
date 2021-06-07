d963a0bad9c55c1db540269de1ce5f15a958f1bd
Rahat-Khan-Pathan <85352119+Rahat-Khan-Pathan@users.noreply.github.com>
2021-06-07 14:38:35 +0600

    Add files via upload

---
+++ 11764 - Jumping Mario.cpp
@@ -0,0 +1,49 @@
+/*
+||-----------------------------------||
+||        Rahat Khan Pathan          ||
+||      BGC Trust University         ||
+||       Department of CSE           ||
+||-----------------------------------||
+*/
+#include<bits/stdc++.h>
+#define ll long long int
+#define pub push_back
+#define pi 3.14159265359
+#define vll vector<ll>
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique(All(X)),(X).end()))
+#define fast ios_base::sync_with_stdio(0); cin.tie(0);
+#define pi pair<ll,ll>
+#define vpair vector<pair<ll,ll> >
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define minus cout<<-1<<endl
+using namespace std;
+ll ts=1;
+void solve()
+{
+    ll n,i,high=0,low=0;
+    cin>>n;
+    ll a[n+1];
+    for(i=0;i<n;i++)cin>>a[i];
+    for(i=1;i<n;i++)
+    {
+        if(a[i]>a[i-1])
+            high++;
+        if(a[i]<a[i-1])
+            low++;
+    }
+    cout<<"Case "<<ts<<": "<<high<<" "<<low<<endl;
+    ts++;
+}
+int main()
+{
+    fast;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
