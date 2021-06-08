c47ba41288ab7783262b6a6c91b834eae9b687f8
Rahat-Khan-Pathan <rahatkhanpathan99@gmail.com>
2021-06-09 00:02:02 +0600

    Add files via upload

---
+++ 1585 - Score.cpp
@@ -0,0 +1,43 @@
+/*
+||-----------------------------------||
+||        Rahat Khan Pathan          ||
+||      BGC Trust University         ||
+||       Department of CSE           ||
+||-----------------------------------||
+*/
+#include<bits/stdc++.h>
+#define ll long long int
+#define pub push_back
+#define PI 3.14159265359
+#define vll vector<ll>
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique(All(X)),(X).end()))
+#define fast ios_base::sync_with_stdio(0); cin.tie(0);
+#define pi pair<ll,ll>
+#define vpair vector<pair<ll,ll> >
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define minus cout<<-1<<endl
+using namespace std;
+int main()
+{
+    fast;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        string a;
+        ll i,ans=0,c=0;
+        cin>>a;
+        for(i=0;i<a.size();i++)
+        {
+            if(a[i]=='O')
+                c++;
+            else
+                c=0;
+            ans+=c;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
