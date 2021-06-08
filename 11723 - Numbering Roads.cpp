89bed63b7387fedde763465c675a09a3bd244290
Rahat-Khan-Pathan <rahatkhanpathan99@gmail.com>
2021-06-08 14:34:44 +0600

    Add files via upload

---
+++ 11723 - Numbering Roads.cpp
@@ -0,0 +1,36 @@
+/*
+||-----------------------------------||
+||        Rahat Khan Pathan          ||
+||      BGC Trust University         ||
+||       Department of CSE           ||
+||-----------------------------------||
+*/
+#include<bits/stdc++.h>
+#define ll long long int
+#define pub push_back
+#define PI 3.14159265359
+#define vll vector<ll>
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique(All(X)),(X).end()))
+#define fast ios_base::sync_with_stdio(0); cin.tie(0);
+#define pi pair<ll,ll>
+#define vpair vector<pair<ll,ll> >
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define minus cout<<-1<<endl
+using namespace std;
+ll ts=1;
+int main()
+{
+    fast;
+    ll a,b;
+    while(cin>>a>>b)
+    {
+        if(a==0 && b==0) break;
+        ll ans;
+        ans=ceil(a*1.0/b)-1;
+        (ans>26)? cout<<"Case "<<ts<<": impossible"<<endl : cout<<"Case "<<ts<<": "<<ans<<endl;
+        ts++;
+    }
+    return 0;
+}
