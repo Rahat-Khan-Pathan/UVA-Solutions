9f28b102d88096ad53c2a8a6fd99b7d997e6df4c
Rahat-Khan-Pathan <rahatkhanpathan99@gmail.com>
2021-06-09 00:52:21 +0600

    Add files via upload

---
+++ 12658 - Character Recognition.cpp
@@ -0,0 +1,44 @@
+/*
+||-----------------------------------||
+||        Rahat Khan Pathan          ||
+||      BGC Trust University         ||
+||       Department of CSE           ||
+||-----------------------------------||
+*/
+#include<bits/stdc++.h>
+#define ll long long int
+#define pub push_back
+#define PI 3.14159265359
+#define vll vector<ll>
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique(All(X)),(X).end()))
+#define fast ios_base::sync_with_stdio(0); cin.tie(0);
+#define pi pair<ll,ll>
+#define vpair vector<pair<ll,ll> >
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define minus cout<<-1<<endl
+using namespace std;
+int main()
+{
+    fast;
+    ll n;
+    while(cin>>n)
+    {
+        string a[6];
+        ll i;
+        for(i=0;i<5;i++)
+            cin>>a[i];
+        for(i=0;i<n;i++)
+        {
+            if(a[0][i*4]=='.')
+                cout<<1;
+            else if(a[3][i*4]=='*')
+                cout<<2;
+            else
+                cout<<3;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
